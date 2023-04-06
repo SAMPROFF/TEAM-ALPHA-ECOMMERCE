81cda40fa61273009096e6436e528068d076fe61
Clementina-dev1 <adjeiclementina@gmail.com>
2023-04-06 20:38:36 +0000

    Update signIn.js

--- signIn.js
+++ signIn.js
@@ -1,72 +1,45 @@
-import { useRef, useState} from "react";
-import {useNavigate} from 'react-router-dom'
-import {toast} from 'reaact-toastify'
-
-const signIn = () => {
-    const userRef = useRef();
-
-    const [email, setEmail] = useState('');
-    const [pwd, setPwd] = useState('');
-    const navigate = useNavigate();
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(email,pwd);
-        const loggedUser = JSON.parse(localStorage.getItem('user'));
-          console.log(loggedUser)
-           if(email===loggedUser.email && pwd===loggedUser.pwd){
-             localStorage.setItem('isloggedIn',true)
-              navigate('/')
-          }else{
-             toast.error('Wrong username or password')
-         };
-    };
-
-    return (
-        <div className='forms'>
-
-             <h1>Sign in</h1>
-
-             <form onSubmit={handleSubmit}>
-
-                <div className='form-section'>
-                  <label htmlFor="email">Email Address</label>
-                     <input 
-                       type="text"
-                       id="email"
-                       ref={userRef}
-                       autoComplete="off"
-                       onChange={(e) => setEmail(e.target.value)}
-                       value={email} 
-                       required 
-                      />
-                    </div>    
-                 
-                 <div className='form-section'>
-                     <label htmlFor="passwword">Password</label>
-                       <input 
-                         type="password"
-                         id="password"
-                         onChange={(e) => setPwd(e.target.value)}
-                         value={pwd} 
-                         required
-                       />
-                    </div>   
-                
-                <div className='form-section'>
-                    <button className='btn'>Sign in</button>
-                </div>
-
-            </form>
-      </div>
-            
-        
-
-    );
-};
-
-
-
-
-export default signIn
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (email === 'user@example.com' && password === 'password') {
+      localStorage.setItem('isLoggedIn', true);
+      history.push('/');
+    } else {
+      alert('Invalid email or password');
+    }
+  }
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+export default Login;
